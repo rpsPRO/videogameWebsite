cb194df52699d0fe5ced286b7fb213b9397c8026
rpsPRO <rplazassanchez@gmail.com>
2022-01-14 18:26:19 +0100

    HelloWorld

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const greeting = <h1>Hello World!</h1>;
+
+
+ReactDOM.render(
+  greeting,
+  document.getElementById('root')
+);
+
+
